5e53a9119144c34a24f5d1528809db024d52f40b
Marrit <marritleenstra@hotmail.com>
2017-09-24 22:08:07 +0200

    Initial commit

---
+++ Song.java
@@ -0,0 +1,64 @@
+package marrit.marritleenstra_pset3;
+
+
+/**
+ * Created by Marrit on 24-9-2017.
+ * Class that holds all the information about a song.
+ */
+
+public class Song {
+
+
+    private String mId;
+    private String mTitle;
+    private String mArtist;
+    private String mAlbum;
+    private String mGenre;
+
+
+    // initiate instance of Class
+    public Song() {
+
+    }
+
+    // getters and setters for all fields
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getArtist() {
+        return mArtist;
+    }
+
+    public void setArtist(String artist) {
+        mArtist = artist;
+    }
+
+    public String getAlbum() {
+        return mAlbum;
+    }
+
+    public void setAlbum(String album) {
+        mAlbum = album;
+    }
+
+    public String getGenre() {
+        return mGenre;
+    }
+
+    public void setGenre(String genre) {
+        mGenre = genre;
+    }
+}
